e61413b34a4a43d8e03891f0fc372e866ec0797c
Aleksaachi <aleksanderatanasov60@gmail.com>
2023-08-13 21:56:34 +0300

    Playing cards

--- Assets/Resources/Scripts/Deck.cs
+++ Assets/Resources/Scripts/Deck.cs
@@ -5,7 +5,29 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
     public List<Card> cards = new();
+    public List<Card> cardsInHand = new();
+    GameObject cardInHandPrefab;
+    public GameObject cardInCombatPrefab;
+    public Transform canvasTransform;
 
+    public Transform selectedCard;
+
+    private void Start()
+    {
+        cardInHandPrefab = Resources.Load<GameObject>("Prefabs/CardInHand");
+        cardInCombatPrefab = Resources.Load<GameObject>("Prefabs/CardInCombat");
+        canvasTransform = GameObject.Find("Canvas").transform;
+    }
+
+    private void Update()
+    {
+        if (selectedCard != null)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedCard.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+        }
+
+    }
     public void AddCard(Card card){
         cards.Add(card);
         PrintDeck();
@@ -36,6 +58,18 @@ public class Deck : MonoBehaviour
         Debug.Log(cardsInDeck);
     }
 
+    // Teglene na karti v //  
+    public void DrawCard()
+    {
+        cardsInHand.Add(cards[0]);
+        var card = Instantiate(cardInHandPrefab, new Vector3(cardsInHand.Count * 2, -3.5f, 0), Quaternion.identity);
+        card.transform.SetParent(canvasTransform);
+        card.transform.localScale = Vector3.one;
+        CardInHand cardInHan = card.GetComponent<CardInHand>();
+        cardInHan.card = cardsInHand[cardsInHand.Count - 1];
+        cardInHan.deck = this;
+        cards.RemoveAt(0);
+    }
 
     //Shuffle the deck using the Fisher-Yates shuffle
     System.Random random = new System.Random();
