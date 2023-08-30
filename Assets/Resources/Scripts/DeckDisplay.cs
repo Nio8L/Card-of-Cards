991319a6029b7db7e9270a348bb4388d3d7a8551
Nio8L <nio8.minecraft@gmail.com>
2023-08-30 23:25:05 +0300

    Deck Display (press D)

---
+++ Assets/Resources/Scripts/DeckDisplay.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckDisplay : MonoBehaviour
+{
+    public GameObject cardDisplay;
+    public Deck deck;
+    public GameObject deckDisplay;
+
+    private List<GameObject> cardDisplays;
+
+    private void Start() {
+        cardDisplays = new();
+    }
+
+    private void Update() {
+        if(Input.GetKeyUp(KeyCode.D)){
+            ShowDeck();
+        }    
+    }
+
+    private void ShowDeck(){
+        if(cardDisplays.Count > 0){
+            foreach(GameObject cardDisplay in cardDisplays){
+                Destroy(cardDisplay);
+            }
+        }
+        cardDisplays.Clear();
+        
+        deckDisplay.SetActive(!deckDisplay.activeSelf);
+
+        foreach (Card card in deck.cards)
+        {   
+            Debug.Log(transform.position.x);
+            GameObject newCardDisplay = Instantiate(cardDisplay, new Vector3(cardDisplays.Count % 5 * 2 - 4,  -cardDisplays.Count / 5 * 3 + 3, transform.position.z), Quaternion.identity);
+            newCardDisplay.GetComponent<CardDisplay>().card = card;
+            newCardDisplay.transform.SetParent(deckDisplay.transform);
+            newCardDisplay.transform.localScale = Vector3.one;
+
+            cardDisplays.Add(newCardDisplay);
+        }
+    }
+}
