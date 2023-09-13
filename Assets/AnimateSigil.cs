019e5f451b6ae91e2c4d17d9bc85a8e1fc698d8c
Aleksaachi <aleksanderatanasov60@gmail.com>
2023-09-13 18:50:07 +0300

    Sigil reworks and new hunt AIs

--- Assets/AnimateSigil.cs
+++ Assets/AnimateSigil.cs
@@ -7,7 +7,7 @@ public class AnimateSigil : MonoBehaviour
     float maxTime = 0.5f;
     float t = -1;
     float scale = 1;
- 
+    bool updating = false;
     void Update()
     {
         if (t > -maxTime / 2)
@@ -17,10 +17,18 @@ public class AnimateSigil : MonoBehaviour
             else scale -= 2 * Time.deltaTime;
             transform.localScale = Vector3.one * scale;
         }
+        else if (updating)
+        {
+            updating = false;
+            scale = 1;
+            transform.localScale = Vector3.one;
+        }
+
     }
 
     public void StartAnimation()
     {
+        updating = true;
         t = maxTime / 2;
     }
 }
