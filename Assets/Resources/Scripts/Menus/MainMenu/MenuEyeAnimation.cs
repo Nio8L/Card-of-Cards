0afcef972822a14285b59ffb21463b6bccf7257e
Nio8L <nio8.minecraft@gmail.com>
2023-09-20 01:47:26 +0300

    Menu eye blinks randomly

---
+++ Assets/Resources/Scripts/Menus/MainMenu/MenuEyeAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEyeAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    public void FinishInitialize(){
+        LeanTween.cancelAll();
+        animator.SetBool("Blink", true);
+        Blink(0);
+    }
+
+    public void Blink(float beginAfter){
+
+        LeanTween.delayedCall(beginAfter, () => {
+            float animateAfter = Random.Range(2, 7);
+
+            //Debug.Log(animateAfter);
+            animator.Play("Base Layer.MenuEyeBlink", 0, 0f);
+            Blink(animateAfter);
+        });
+    }
+
+    private void OnDisable() {
+        LeanTween.cancelAll();
+    }
+}
