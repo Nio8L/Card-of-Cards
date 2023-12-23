bb941dc403ebd3eb59c8080d70079dd6030dad4c
Nio8L <nio8.minecraft@gmail.com>
2023-12-23 18:52:05 +0200

    Exchange event bugfixes and Hippo balancing
    The exchange event now starts with the first offer selected.
    
    The deck display is now always set to the correct position in the exchange event.

--- Assets/Resources/Scripts/Events/ExchangeShop.cs
+++ Assets/Resources/Scripts/Events/ExchangeShop.cs
@@ -21,18 +21,25 @@ public class ExchangeShop : MonoBehaviour
         //Reposition the deck display and open it
         if (!MapManager.mapManager.deckDisplay.deckDisplay.activeSelf)
         {
-            MapManager.mapManager.deckDisplay.deckDisplay.GetComponent<RectTransform>().localPosition = new Vector3(450, 0, 0);
             MapManager.mapManager.deckDisplay.ShowDeck();
             MapManager.mapManager.deckDisplay.canClose = false;
+            MapManager.mapManager.deckDisplay.deckDisplay.GetComponent<RectTransform>().localPosition = new Vector3(450, 0, 0);
         }
 
+        //Generate offers
         for (int i = 0; i < 3; i++)
         {
             Card card = PickCard(i);
             CardDisplay cardDisplay = transform.GetChild(1).GetChild(i).GetComponent<CardDisplay>();
             cardDisplay.card = card;
             cardDisplay.UpdateCardAppearance();
+
+            if(i == 0){
+                cardDisplay.SelectCardForSacrifice();
+            }
         }
+
+
     }
 
     Card PickCard(int value)
@@ -98,18 +105,30 @@ public class ExchangeShop : MonoBehaviour
         cardSlotHandler.DisableAllSlots();
 
         if (offeredCard.GetComponent<CardOffered>().price == 1){
-            cardSlotHandler.DisableSlot(0);
-            cardSlotHandler.EnableSlot(1);
-            cardSlotHandler.DisableSlot(2);
+            SelectExchange1();
         }else if (offeredCard.GetComponent<CardOffered>().price == 2){
-            cardSlotHandler.EnableSlot(0);
-            cardSlotHandler.DisableSlot(1);
-            cardSlotHandler.EnableSlot(2);
+            SelectExchange2();
         }else{
-            cardSlotHandler.EnableSlot(0);
-            cardSlotHandler.EnableSlot(1);
-            cardSlotHandler.EnableSlot(2);
+            SelectExchange3();
         }
+    }
+
+    private void SelectExchange1(){
+        cardSlotHandler.DisableSlot(0);
+        cardSlotHandler.EnableSlot(1);
+        cardSlotHandler.DisableSlot(2);
+    }
+
+    private void SelectExchange2(){
+        cardSlotHandler.EnableSlot(0);
+        cardSlotHandler.DisableSlot(1);
+        cardSlotHandler.EnableSlot(2);
+    }
+
+    private void SelectExchange3(){
+        cardSlotHandler.EnableSlot(0);
+        cardSlotHandler.EnableSlot(1);
+        cardSlotHandler.EnableSlot(2);
     }
 
     //Should be moved to the MapManager so it can work for all events
